558b702f6e85a0db81d2b46e914ae70a29106e32
Iamthecheyenne <60201731+Iamthecheyenne@users.noreply.github.com>
2021-04-07 19:55:19 -0400

    update

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,3 +1,12 @@
-object.onclick = function(){myScript};
 
+window.addEventListener('keydown', function(Event){
+    if (Event.keyCode == 32){
+        <span class="Spacebar">
+        Press the spacebar to continue
+
+        </span>
+        console.log('Space pressed');
+
+    }
+});
 
